4054f34ca8d60d502f78a2045ce06da902082780
Rajneesh <105125534+AF0182311@users.noreply.github.com>
2022-08-06 21:44:06 +0530

    Update NotMatched.java

--- NotMatched.java
+++ NotMatched.java
@@ -1,5 +1,12 @@
+/* Write a program to accept basic details name , Phone No , Address ,
+user Id and password from the user . 
+throw an exception if the user Id lenght is greater than 8 
+and password is not matched and should only contain the string value.
+Password rules:
+A password must have at least eight characters.
+A password consists of only letters and digits.
+A password must contain at least two digits. */
 package com.lab;
-
 public class NotMatched extends RuntimeException{
 
 	NotMatched(String s)
